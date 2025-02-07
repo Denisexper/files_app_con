d680d6196c312ef4260e2edea2427ac1f52f1a5a
Denisexper <clasic.exper2023@gmail.com>
2025-02-07 14:22:51 -0600

    add: hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const APP_STATUS = {
+  IDLE: 'idle', // al entrar 
+  ERROR: 'error', // cuando hay un error
+  READY_UPLOAD: 'ready_upload', // al elegir el archivo y antes de subir
+  UPLOADING: 'uploading', // meintras se sube
+  READY_USAGE: 'ready_usage', // despues de subir
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [appStatus, setAppStatus] = useState(APP_STATUS.IDLE)
+  const [file, setFile] = useState(null)
+
+  const handleInputChange = (event) => {
+    const [file] = event.target.files ?? []
+    if(file) {
+      setFile(file)
+      setAppStatus(APP_STATUS.READY_UPLOAD)
+    }
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('TODO')
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h4> Challenge: Upload CSV + Searach</h4>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            onChange={handleInputChange}
+            name='file'
+            type='file'
+            accept='.csv'
+          />
+        </label>
+
+        {appStatus === APP_STATUS.READY_UPLOAD && (<button>
+          Subir archivo
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        )}
+      </form>
     </>
   )
 }
